89bfded183f9cb5e149e53dbe20f1d6f5e403522
shuvo85cisco <shuvo85cste@gmail.com>
2021-03-31 11:24:19 +0800

    Update pmisreporting.sh

--- pmisreporting.sh
+++ pmisreporting.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#php  /data/html/pmisdev.sebpo.net/weekly_reports/report.php
-php /var/www/html/pmis.sebpo.net/dev_weekly_utilization/report.php
+php /var/www/html/dev_weekly_utilization/report.php
